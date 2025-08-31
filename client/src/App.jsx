a8177bd111bce6cb48823b7513ef967dde6ce134
daniyal-shams <hussaindaniyal748@gmail.com>
2025-08-31 08:50:49 +0530

    Project Init

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Background from './Components/Backgorund'
+
+const App = () => {
+  return (
+    <div className='relative w-full h-screen'>
+      <Background />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/Components/Backgorund.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef, useState } from "react";
+
+function Background() {
+const vantaRef = useRef(null);
+  const [vantaEffect, setVantaEffect] = useState(null);
+
+  useEffect(() => {
+    if (!vantaEffect && window.VANTA) {
+      setVantaEffect(
+        window.VANTA.NET({
+          el: vantaRef.current,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1.0,
+          scaleMobile: 1.0,
+          color: 0xdd3fff,
+          maxDistance: 13.0,
+          spacing: 11.0,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
+  return <div ref={vantaRef} className="w-full h-screen"></div>;}
+
+export default Background;
